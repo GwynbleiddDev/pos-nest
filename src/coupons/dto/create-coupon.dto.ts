eed0b32cea894bfac54fe861cd3b52e2bfea73c8
Alejandro Valera <ale24vv@gmail.com>
2025-06-21 17:32:23 -0400

    fix message coupon

--- src/coupons/dto/create-coupon.dto.ts
+++ src/coupons/dto/create-coupon.dto.ts
@@ -6,7 +6,7 @@ export class CreateCouponDto {
   @IsNotEmpty({message: 'El nombre del cupón es obligatorio'})
   name: string
 
-  @IsNotEmpty({message: 'Eñ descuento no puede ir vacio'})
+  @IsNotEmpty({message: 'El descuento no puede ir vacio'})
   @IsInt({message: 'El descuento debe ser entre 1 y 100'})
   @Max(100, {message: 'El descuento máximo es de 100'})
   @Min(1, {message: 'El descuento mínimo es de 1'})
